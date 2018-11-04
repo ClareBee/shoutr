ae19dfc1a80440330b918ff704d08954d587820e
Clare <chblackburne@gmail.com>
2018-11-04 16:57:22 +0000

    Add guest methods for if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  def current_user
+    # calls original implementation w fallback of guest
+    super || Guest.new
+  end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,8 @@ class Guest < ApplicationRecord
   def email
     ""
   end
+
+  def liked?(*args)
+    return false
+  end
 end
