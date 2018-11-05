2b4cbd65c5684d289c374ddc8da6f9ce5cdce8ca
Clare <chblackburne@gmail.com>
2018-11-05 09:56:19 +0000

    Possible refactor onto model scope

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,8 +1,6 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = params[:id]
-    # polymorphic therefore manual
-    # ? to avoid sql injection
-    @results = Shout.joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").where("text_shouts.body LIKE ?", "%##{@hashtag}%")
+    @results = Shout.search(term: "##{@hashtag}")
   end
 end
